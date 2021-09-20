d9a5b7bed5a1d5db386e14d16ef7a89a967ff8a7
harishpallapu <81285240+harishpallapu@users.noreply.github.com>
2021-09-20 20:53:50 +0530

    Update parameters.groovy

--- parameters.groovy
+++ parameters.groovy
@@ -4,4 +4,4 @@ env.FUNCTIONAL_WEB_GIT_URL="https://github.com/harishpallapu/webtest.git"
 env.FUNCTIONAL_MOBILE_GIT_URL="https://github.com/harishpallapu/mobiletest.git"
 env.JMETER_GIT_URL="https://github.com/harishpallapu/folder.git"
 env.TOMCAT_IP="13.235.133.32"
-env.NEXUS_IP= 
+env.NEXUS_IP=
