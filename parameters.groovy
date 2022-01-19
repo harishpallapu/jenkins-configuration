23ff183f16071b7d930b387c3329d3eaf4dc80df
harishpallapu <81285240+harishpallapu@users.noreply.github.com>
2022-01-19 11:39:25 +0530

    Update parameters.groovy

--- parameters.groovy
+++ parameters.groovy
@@ -4,6 +4,6 @@ env.tomcat_ip="http://mypublicip:8084"
 env.functional_web_git_url="https://github.com/harishpallapu/webtest.git"
 env.functional_mobile_git_url="https://github.com/harishpallapu/mobiletest.git"
 env.dast_ip="http://mypublicip:8084/SpringMVCAnnotationShoppingCart/"
-env.jmeter_git_url="htps://github.com/harishpallapu/folder.git"
+env.jmeter_git_url="https://github.com/harishpallapu/folder.git"
 env.nexus_ip="http://localhost:8081"
 
