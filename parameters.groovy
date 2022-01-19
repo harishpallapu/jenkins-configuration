5e3d448b83e982ecbb147dcc7a78bb7e9f722d25
harishpallapu <81285240+harishpallapu@users.noreply.github.com>
2022-01-19 13:22:20 +0530

    Update parameters.groovy

--- parameters.groovy
+++ parameters.groovy
@@ -1,7 +1,9 @@
 env.SAST_GIT_URL=
+env.SCA_URL="https://github.com/jeremylong/DependencyCheck/releases/download/v6.2.2/dependency-check-6.2.2-release.zip"
 env.BUILD_GIT_URL="https://github.com/harishpallapu/SourceCode_SAST.git"
 env.TOMCAT_IP="http://mypublicip:8084"
-
+env.FUNCTIONAL_WEB_GIT_URL="https://github.com/harishpallapu/webtest.git"
 env.FUNCTIONAL_MOBILE_GIT_URL=
+env.DAST_URL="http://mypublicip:8084/SpringMVCAnnotationShoppingCart/"  
 env.JMETER_GIT_URL="https://github.com/harishpallapu/folder.git"
 env.NEXUS_IP="http://localhost:8081"
