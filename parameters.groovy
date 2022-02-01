f37e26b9e731d84cbb550e8b8ac15662272ba0b3
harishpallapu <81285240+harishpallapu@users.noreply.github.com>
2022-02-01 14:16:47 +0530

    Update parameters.groovy

--- parameters.groovy
+++ parameters.groovy
@@ -1,4 +1,4 @@
-env.BRANCH=
+env.BRANCH="test"
 env.SAST_GIT_URL="https://github.com/harishpallapu/SourceCode_SAST.git"
 env.BUILD_GIT_URL="https://github.com/harishpallapu/SourceCode_SAST.git"
 env.TOMCAT_IP="http://mypublicip:8084"
